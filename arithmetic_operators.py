f1d1fa60b51020933f079ffe0f9e20e92fcad212
Prakash Loganathan <prakash.loganathaan@gmail.com>
2022-12-27 15:19:48 -0500

    Added arithmetic operator code

---
+++ arithmetic_operators.py
@@ -0,0 +1,18 @@
+# Arithmetic operators
+# + Addition
+# - Subtraction
+# * Multiplication
+# / Division
+# % Mod (the remainder after dividing)
+# ** Exponentiation (note that ^ does not do this operation, as you might have seen in other languages)
+# // Divides and rounds down to the nearest integer
+
+# PEMDAS: Parentheses, Exponents, Multiplication, Division, Addition, Subtraction.
+
+# Video
+# https://www.youtube.com/watch?v=M8TIOK2P2yw&t=49s&ab_channel=Udacity
+
+print(3+5) #8
+print(1+2+3*3) #12
+print(3 ** 2) #9
+print(9 % 2) # 1
