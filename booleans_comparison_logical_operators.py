cf68c69aa3b25e14196fe8bcd3138ef285ef503d
Prakash Loganathan <prakash.loganathaan@gmail.com>
2022-12-27 21:14:20 -0500

    Added booleans, comparison, and logical operators code

---
+++ booleans_comparison_logical_operators.py
@@ -0,0 +1,14 @@
+# Video
+# https://www.youtube.com/watch?v=iNNsUJIDtVU&t=37s&ab_channel=Udacity
+
+x = 42 > 43
+print(x)
+
+age = 14
+is_teen = age > 12 and age < 20
+print(is_teen)
+
+# NOT logical operator
+age = 12
+is_not_teen = not(age > 12 and age < 20)
+print(is_not_teen)
